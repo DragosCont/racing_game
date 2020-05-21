d2db7e035938ed1a5c01aee4f2a97c9a85508d18
Dragos Cont <dragoscont@live.com>
2020-05-21 21:31:15 +0300

    added Car class

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,19 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String make;
+    String model;
+    String color;
+    double mileage;
+    double fuelLevel;
+    double maxSpeed;
+    double totalTraveledDistance;
+
+    boolean damaged;
+    int doorCount;
+
+
+
+
+}
